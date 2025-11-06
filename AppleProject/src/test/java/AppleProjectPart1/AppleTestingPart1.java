0b34565b7f038a3d3097e3e62b6d5b8bc41abe0a
rogers <rogers@localhost>
2025-11-06 08:39:27 +0530

    Even number added

---
+++ AppleProject/src/test/java/AppleProjectPart1/AppleTestingPart1.java
@@ -0,0 +1,28 @@
+package AppleProjectPart1;
+
+public class AppleTestingPart1 {
+
+	public static void main(String[] args) 
+	{
+		int a = 1;
+		
+		while(a<=23)
+		{
+			System.out.println(a);
+			a++;
+		}
+		
+		// to print even numbers 
+		
+		int b = 2;
+		
+		while(b<=23)
+		{
+			System.out.println(b);
+			b+=2;
+		}
+
+
+	}
+
+}
